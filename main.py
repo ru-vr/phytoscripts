c9ee9e31c981dc012aaefaa71cd7c12cab57f5aa
rv <88005814+ru-vr@users.noreply.github.com>
2023-06-05 15:38:53 +0200

    nova verze s poznavackou botaniky

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
 #app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
 
 def get_db_connection():
-    connection = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'],
-                                 password=os.environ['DB_PASSWORD'])
+    #connection = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'],
+      #                           password=os.environ['DB_PASSWORD'])
+    connection = psycopg2.connect(host='localhost', database='diplomky', user='postgres',
+                                     password='loooll')
     return connection
 
 @app.route('/')
@@ -37,8 +42,8 @@ def diplomky():
 def alkaloid(alk):
     connection = get_db_connection()
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM alkaloidy WHERE nazev = %s', (alk, ))  # vybere jen jeden radek??
-    alkaloid = cursor.fetchall()  # a vsechna data v nem ??
+    cursor.execute('SELECT * FROM alkaloidy WHERE nazev = %s', (alk, ))  # vybere jen jeden radek
+    alkaloid = cursor.fetchall()  # a vsechna data v nem
     cursor.close()
     connection.close()
     return render_template('alkaloid.html', alkaloid=alkaloid)
@@ -60,6 +65,21 @@ def kfgfb():
 @app.route('/otacivost')
 def otacivost():
     return render_template('otacivost.html')
+
+@app.route('/poznavacka-botanika', methods=['GET', 'POST'])
+def poznavacka_botanika():
+   # if request.method == 'POST':
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM poznavacka_botanika ORDER BY random() LIMIT 15')
+    vyber = cursor.fetchall()  #
+    cursor.close()
+    connection.close()
+    return render_template('poznavacka-botanika.html', vyber=vyber)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
