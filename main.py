839faf2d45cab4141e1aa07a6b37c3900a80f2a1
rv <88005814+ru-vr@users.noreply.github.com>
2023-06-27 16:07:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import psycopg2
 import random
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from urllib.parse import unquote
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -11,9 +12,10 @@ Bootstrap(app)
 #app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
 
 def get_db_connection():
-    connection = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'],
-                                password=os.environ['DB_PASSWORD'])
-
+    #connection = psycopg2.connect(host=os.environ['DB_HOST'], database=os.environ['DB_NAME'], user=os.environ['DB_USER'],
+      #                           password=os.environ['DB_PASSWORD'])
+    connection = psycopg2.connect(host='localhost', database='diplomky', user='postgres',
+                                     password='loooll')
     return connection
 
 @app.route('/')
@@ -38,7 +40,7 @@ def diplomky():
     connection.close()
     return render_template('diplomky.html', seznam_diplomek=seznam_diplomek)
 
-@app.route('/<alk>')
+@app.route('/alkaloid/<alk>', methods=['GET', 'POST'])
 def alkaloid(alk):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -47,6 +49,18 @@ def alkaloid(alk):
     cursor.close()
     connection.close()
     return render_template('alkaloid.html', alkaloid=alkaloid)
+
+@app.route('/alkaloid/', methods=['GET', 'POST'])
+def find_alkaloid():
+    if request.method == 'POST':
+        hledany_alk = request.form['hledej_alk']
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM alkaloidy WHERE nazev = %s', (hledany_alk,))  # vybere jen jeden radek
+        alkaloid = cursor.fetchall()  # a vsechna data v nem
+        cursor.close()
+        connection.close()
+        return render_template('alkaloid.html', alkaloid=alkaloid, hledany_alk=hledany_alk)
 
 @app.route('/alkaloidy')
 def alkaloidy():
@@ -157,14 +171,8 @@ def macro_gnosy():
         soubory_v_jedne_polozce = os.listdir(polozka)
         random.shuffle(soubory_v_jedne_polozce) # nahodné promíchání obrazků
         nahodny_1_soubor = soubory_v_jedne_polozce.pop()
-        try:
-            nahodny_2_soubor = soubory_v_jedne_polozce.pop()
-        except:
-            print(polozka + " je chybná")
-        try:
-            nahodny_3_soubor = soubory_v_jedne_polozce.pop()
-        except:
-            print(polozka + " je chybná")
+        nahodny_2_soubor = soubory_v_jedne_polozce.pop()
+        nahodny_3_soubor = soubory_v_jedne_polozce.pop()
         cesta_1 = polozka + "/" + nahodny_1_soubor
         cesta_2 = polozka + "/" + nahodny_2_soubor
         cesta_3 = polozka + "/" + nahodny_3_soubor
@@ -173,6 +181,29 @@ def macro_gnosy():
     cursor.close()
     connection.close()
     return render_template('macro-gnosy.html', vyber=vyber, paths=array_of_all_pahts)
+
+@app.route('/foto-botanika')
+def foto_botanika():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT druh_latinsky FROM poznavacka_botanika')
+    seznam_nazvu = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    seznam_nazvu = [item[0].strip() for item in seznam_nazvu]
+    seznam_nazvu= sorted([s.replace("\xa0", "") for s in seznam_nazvu])
+    return render_template('foto-botanika.html', seznam_nazvu=seznam_nazvu)
+
+@app.route('/rostlina/<konkretni>')
+def nazev(konkretni):
+    konkretni = unquote(konkretni)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM poznavacka_botanika WHERE druh_latinsky = %s', (konkretni, ))
+    rostlina = cursor.fetchall()  # a vsechna data v nem
+    cursor.close()
+    connection.close()
+    return render_template('foto-rostlina.html', rostlina=rostlina)
 
 @app.route('/test')
 def test():
